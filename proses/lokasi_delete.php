dd799d6fa022e4c1eb4e01c0d37bd60caf3f887f
LENOVO\Ken <krangga23@gmail.com>
2021-01-11 23:00:09 +0700

    commit lokasi_delete
    lokasi_delete

---
+++ proses/lokasi_delete.php
@@ -0,0 +1,20 @@
+<?php 
+	
+	if(isset($_GET['id'])){
+
+		include '../koneksi.php';
+
+		$query  = "DELETE FROM lokasi WHERE id='".$_GET['id']."'";
+
+		$tambah = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
+		if($tambah){
+			$_SESSION['alert'] = '<div class="alert alert-success">Data berhasil dihapus</div>';
+			
+		}else{
+			$_SESSION['alert'] = '<div class="alert alert-danger">Data gagal dihapus</div>';
+		}
+
+		header('Location:../admin_lokasi.php');
+	}
+	
+?>
